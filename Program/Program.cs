36b3e2696a229f9923b478e256dbf86b651214be
Illia Shakin <S00188372@mail.itsligo.ie>
2020-11-26 14:11:38 +0000

    Added Player class with 3 properties

---
+++ Program/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    public class Program
+    {
+
+        public int Id { get; set; }
+        public String PlayerName { get; set; }
+        public int Score { get; set; }
+    }
+}

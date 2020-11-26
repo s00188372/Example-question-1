fdbfbbedffa6145254f9c80a241111c9616bd3d7
Illia Shakin <S00188372@mail.itsligo.ie>
2020-11-26 14:18:46 +0000

    Adding Players

--- Program/Program.cs
+++ Program/Program.cs
@@ -8,7 +8,15 @@ namespace Program
 {
     public class Program
     {
+        static void Main(string[] args)
+        {
+            Player player1 = new Player(1, "Millie", 0);
+            Player player2 = new Player(2, "Katie", 0);
+            Player player3 = new Player(3, "Kevin", 0);
+            Player player4 = new Player(4, "Conor", 0);
+            Player player5 = new Player(5, "Pauline", 0);
 
+        }
 
 
     }
